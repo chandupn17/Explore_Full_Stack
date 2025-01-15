681677cf7ecdf9a7ad14208dafa06134200d0435
hithesh143life@gmail.com <1bytecode.git@gmail.com>
2025-01-15 23:41:44 +0530

    set up multer and cloudinery set up

---
+++ backend-level02/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY, 
+    api_secret: proccesss.env.CLOUD_API_KEY 
+});
+const uplodeonCoud = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+        // uplode the file on coludinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        // file has been uploded success
+        console.log("file has uploded on coudinary", response.url);
+        return response;
+
+    }catch(error){
+        // if not uploded , to safe clean 
+        fs.unlinkSync(localFilePath);
+        // remove the locally saved temporary file as the uplode operation got failed
+        return null;
+
+    }
+
+};
+export {uplodeonCoud}
