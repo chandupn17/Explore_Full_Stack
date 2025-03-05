4e9d025506ed6fd7ebe8ab1f1bc66030502edc2f
hithesh143life@gmail.com <1bytecode.git@gmail.com>
2025-03-05 21:34:21 +0530

    introduction to tailwind

---
+++ backend-level03/client/index.js
@@ -0,0 +1,8 @@
+let modeBtn = document.querySelectorAll(".mode");
+let mode = "ligtht";
+modeBtn.modeBtn.addEventListener("click", ()=>{
+    if(mode == "light"){
+        
+    }
+console.log("the button is clicked ");
+});
